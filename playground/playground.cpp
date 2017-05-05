a770e38e6c6910530ca572f48a1916f0199cf2af
Alfred Tarng <atarng.A5T@gmail.com>
2017-05-04 17:49:03 -0700

    Tutorial 2 completed.

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,13 +1,29 @@
+// Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 
+// Include GLEW
 #include <GL/glew.h>
 
+// Include GLFW
 #include <glfw3.h>
 GLFWwindow* window;
 
+#include <iostream>
+
+// Include GLM (MATH)
 #include <glm/glm.hpp>
 using namespace glm;
+
+
+/*** Custom Includes ***/
+
+#include <common/shader.hpp>
+using namespace std;
+
+/*** ***/
+
+
 
 int main( void )
 {
@@ -20,16 +36,17 @@ int main( void )
 	}
 
 	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_RESIZABLE,GL_FALSE); //
 
 	// Open a window and create its OpenGL context
 	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
 	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible."
+            "Try the 2.1 version of the tutorials.\n" );
 		getchar();
 		glfwTerminate();
 		return -1;
@@ -37,6 +54,7 @@ int main( void )
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+    glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -50,8 +68,58 @@ int main( void )
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+    //cout << "Press Escape to Exit Terminal" << endl;
+
+    /*** Start Tutorial 2A ***/
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+         0.0f,  1.0f, 0.0f,
+    };
+
+    // This will identify our vertex buffer
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    // The following commands will talk about our 'vertexbuffer' buffer
+    // Give our vertices to OpenGL.
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    /*** End Tutorial 2A ***/
+
 	do{
-		// Draw nothing, see you in tutorial 2 !
+        // Draw nothing, see you in tutorial 2 !
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /*** Start Tutorial 2B ***/
+        // Use our shader
+        glUseProgram(programID);
+
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+        /*** End Tutorial 2B ***/
+
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -60,6 +128,12 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+    // Cleanup VBO
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
